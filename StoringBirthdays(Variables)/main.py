cee12fd5d328ca4bd429903f3072284b57d807c5
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-01 13:45:31 +0530

    Add files via upload

---
+++ StoringBirthdays(Variables)/main.py
@@ -0,0 +1,10 @@
+Birthday1 = 26/1/2010
+print(Birthday1)
+Birthday2= 5/12/2005
+print(Birthday2)
+Birthday3= 4/11/1894
+print(Birthday3)
+Birthday4 = 20/3/2023
+print(Birthday4)
+Birthday5 = 7/10/2013
+print(Birthday5)
